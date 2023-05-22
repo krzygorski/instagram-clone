7264121db8355ef129d556e85ba737c657931800
krzygorski <krzygorski@gmail.com>
2023-05-22 18:51:59 +0200

    feat: added new components

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Post } from "../post/Post";
-
+import { Sidebar } from "../sidebar/Sidebar";
+import "./wrapper.css";
 export const Wrapper = () => {
   return (
-    <div>
-      <Post />
+    <div className="wrapper">
+      <Sidebar />
+      <div className="posts">
+        <Post />
+      </div>
     </div>
   );
 };
